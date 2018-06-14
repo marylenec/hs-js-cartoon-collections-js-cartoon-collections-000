02d929d70ea6d578715b294b619af8d304267298
marylene.chan@gmail.com <marylene.chan@gmail.com>
2018-06-14 23:38:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
-function dwarfRollCall(dwarves) {
+var dwarves = ["Dopey", "Grumpy", "Bashful"];
+
+function dwarfRollCall() {
+  var length = dwarves.length;
+  var msg =[];
+  var position;
+
+  for (var i = 0; i < length; i++) {
+  position = i + 1; 
+  msg += position +  ". " + dwarves[i] + " ";
+  }
+  console.log(msg);
+  return msg;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+dwarfRollCall();
+
+var veggies = ["carrot", "cucumber", "pepper"];
+
+function summonCaptainPlanet(k) {
+  var length = k.length;
+  var msg = "";
+
+    k.forEach(function(k) {
+        msg +=  k.charAt(0).toUpperCase();
+        msg += k.slice(1,9) + "! ";
+      });
+  console.log(length);
+  console.log(msg);
+}
+summonCaptainPlanet(veggies);
+
+function longPlaneteerCalls(l) {
+  var msg = "";
+  if(l.length > 4) {
+    msg = true;
+  } else {
+    msg = false;
+  }
+  console.log(msg);
 }
 
-function longPlaneteerCalls(words) {
+longPlaneteerCalls(longCalls);
+
+var cheddarCheese = ["banana", "cheddar", "sock"];
+var noCheeseArray = ["banana", "tomato", "sock"];
+
+function findTheCheese(m) {
+  var msg = "";
+  var count = 0;
+  for (var i = 0; i < m.length; i++) {
+  var word = m[i];
+  if (word === "cheddar") {
+      count += 1;
+    }
+  if (count === 1) {
+    msg = "cheddar"
+  } else {
+    msg = "no cheese!"
+  }
+  }
+  console.log(count);
+  console.log(msg);
 }
 
-function findTheCheese (foods) {
-}
+findTheCheese(cheddarCheese);
